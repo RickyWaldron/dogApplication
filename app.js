c1bd2df5fd0b2388773938ef20722c1d8e2141f4
Ricky Waldron <ricky_waldron@hotmail.com>
2017-12-18 17:44:13 +0100

    firstday

---
+++ app.js
@@ -0,0 +1,45 @@
+//app.js
+require('dotenv').load();
+const express = require('express')
+const app = express()
+const fs = require('fs')
+var session = require('express-session')
+const bodyParser = require('body-parser')
+const queryParser = require('query-parser')
+const bcrypt = require('bcrypt')
+var port = process.env.port
+
+app.use(session({
+    secret: process.env.secret,
+    resave: true,
+    saveUnitialized: true
+}));
+
+const pg = require('pg')
+const Client = pg.Client
+
+const client = new Client({
+    user: process.env.user,
+    host: 'localhost',
+    database: process.env.database,
+    password: process.env.password,
+    port: 5432,
+})
+
+client.connect()
+app.use(bodyParser.urlencoded())
+app.set('view engine', 'pug')
+require("./signup.js")(app, client)
+// require("./addMessages.js")(app, client)
+// require("./allMessages.js")(app, client)
+// require("./ownMessages.js")(app, client)
+// require("./logout.js")(app)
+// require("./comment.js")(app, client)
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log("listening")
+})
---
+++ signup.js
@@ -0,0 +1,48 @@
+require('dotenv').load();
+const bcrypt = require('bcrypt')
+
+module.exports = (app, client) => {
+		app.get("/signup", (req, res) => {
+			res.render("signup")
+		})
+		app.post('/checkEmail', function(req, res) {
+			let email = req.body.email
+			console.log(email)
+				const query = {
+                  	text: (`SELECT * FROM users WHERE email='${email}'`)
+                    }
+				client.query(query, (error, result) => {
+					console.log(result.rows)
+					if (error) throw error
+					if (result.rows.length == 1){
+					res.send(result)
+					}
+				})
+			})
+		app.post('/signupClass', function(req, res) {
+			let email = req.body.email
+			let firstname = req.body.firstname
+			let lastname = req.body.lastname
+			let age = req.body.age
+			let dogowner = req.body.dogowner
+			let streetAddress =req.body.streetAddress
+			let homeNumber = req.body.homeNumber
+			let city = req.body.city
+			let phoneNumber = req.body.phoneNumber			
+			req.session.email = email
+			req.session.firstname = firstname
+			req.session.lastname = lastname
+			req.session.age = age
+			req.session.dogowner = dogowner
+			req.session.streetAddress = streetAddress
+			req.session.homeNumber = homeNumber
+			req.session.city = city
+			req.session.phoneNumber = phoneNumber
+			res.redirect("/signup2")
+		})
+	}
+
+
+
+//upload.single("picture")
+		
