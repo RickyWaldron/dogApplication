6c32d5db7784891b2ee10fa396ddd8f7e2a8c8e5
Ricky Waldron <ricky_waldron@hotmail.com>
2017-12-20 12:28:34 +0100

    signupFormsWednesday

--- signup.js
+++ signup.js
@@ -6,129 +6,86 @@ module.exports = (app, client) => {
 			res.render("signup")
 		})
 		app.get("/signup2", (req, res) => {
-			res.render("signup2")
+			res.render("signup2", {email: req.session.email})
 		})
-		app.post('/checkEmail', function(req, res) {
+		app.get("/signupInformation", (req, res) => {
+			res.render("signupInformation", {email: req.session.email})
+		})
+		app.post('/signup', function(req, res) {
 			let email = req.body.email
-			console.log(email)
-				const query = {
+			let password = req.body.password
+			const query = {
                   	text: (`SELECT * FROM users WHERE email='${email}'`)
                     }
 				client.query(query, (error, result) => {
-					console.log(result.rows)
+
+					console.log(result.rows.length)
 					if (error) throw error
-					let array = ["key", "value"]
-						
-					if (result.rows.length == 0){
-					res.send(array)
-					}
+						let ajax = req.body.ajax
+						if (result.rows.length == 1){
+
+						let existingUser = result.rows[0].email
+						console.log(existingUser)
+						ajax = false	
+						res.send({existingUser, ajax})		
+						}	
+					else {
+						bcrypt.genSalt(10, function(error, result) {
+						console.log("this is the salt" + result)
+						bcrypt.hash(password, result, function(err, hash) {
+							const query2 = {
+							text: (`INSERT INTO users (email, password) VALUES ('${email}', '${hash}') RETURNING *`)
+							}
+							client.query(query2, (error, result) => {
+								if (error) throw error
+								req.session.email = email
+								res.render("signupInformation")
+								})
+							})
+						})				
+					}	
 				})
+			})	
+	
+		app.post('/signupInformation', function(req, res) {
+			console.log("check for session email: " + req.session.email)
+			let email = req.session.email
+			let firstname = req.body.firstname
+			let lastname = req.body.lastname
+			let age = req.body.age
+			let dogowner = req.body.dogowner
+			let streetAddress =req.body.streetAddress
+			let homenumber = req.body.homeNumber
+			let city = req.body.city
+			let postcode = req.body.postCode
+			let phonenumber = req.body.phoneNumber
+			const query = {
+						text: 	(`UPDATE users SET 
+								firstname='${firstname}', lastname='${lastname}', age='${age}', dogowner='${dogowner}',
+								address='${streetAddress}', homenumber='${homenumber}', city='${city}', postcode='${postcode}',
+								phonenumber='${phonenumber}' WHERE email='${email}'`) 
+						}
+							client.query(query, (error, result) => {
+							if (error) throw error			
+			res.render("signup2", {email: req.session.email})
 			})
-		app.post('/signupForm', function(req, res) {
-			console.log("reached")
-			let email = req.body.email
-			let firstname = req.body.firstname
-			let lastname = req.body.lastname
-			let age = req.body.age
-			let dogowner = req.body.dogowner
-			let streetAddress =req.body.streetAddress
-			let homeNumber = req.body.homeNumber
-			let city = req.body.city
-			let postCode = req.body.postCode
-			let phoneNumber = req.body.phoneNumber			
-			req.session.email = email
-			req.session.firstname = firstname
-			req.session.lastname = lastname
-			req.session.age = age
-			req.session.dogowner = dogowner
-			req.session.streetAddress = streetAddress
-			req.session.homeNumber = homeNumber
-			req.session.city = city
-			req.session.postCode = postCode
-			req.session.phoneNumber = phoneNumber
-			console.log(req.session.email)
-			res.render("signup2", {email: req.session.email, firstname: req.session.firstname,
-			 			lastname: req.session.lastname, age: req.session.age, dogowner: req.session.dogowner,
-			  			streetAddress: req.session.streetAddress, homeNumber: req.session.homeNumber,
-			  			city: req.session.city, postCode: req.session.postCode, phoneNumber: req.session.phoneNumber})
-		})
-		
-		app.post('/signupFormDoglover', function(req, res) {
-			let email = req.body.email
-			let firstname = req.body.firstname
-			let lastname = req.body.lastname
-			let age = req.body.age
-			let dogowner = req.body.dogowner
-			let streetAddress =req.body.streetAddress
-			let homeNumber = req.body.homeNumber
-			let city = req.body.city
-			let postCode = req.body.postCode
-			let phoneNumber = req.body.phoneNumber
-			let username = req.body.username
-			let aboutMe = req.body.about
-			let smallDog = req.body.smallDogButton
-			let mediumDog = req.body.mediumDogButton
-			let largeDog = req.body.largeDogButton
-			req.session.username = username
-			req.session.aboutMe = aboutMe
-			req.session.smallDog = smallDog
-			req.session.mediumDog = mediumDog
-			req.session.largeDog = largeDog
-			res.render("/signupCheck", {email: req.session.email, firstname: req.session.firstname,
-			 			lastname: req.session.lastname, age: req.session.age, dogowner: req.session.dogowner,
-			  			streetAddress: req.session.streetAddress, homeNumber: req.session.homeNumber,
-			  			city: req.session.city, postCode: req.session.postCode, phoneNumber: req.session.phoneNumber,
-			  			username: req.session.username, aboutMe: req.session.aboutMe, smallDog: req.session.smallDog, 
-			  			mediumDog: req.session.mediumDog, largeDog: req.session.largeDog})
 		})
 
+
 		app.post('/signupFormDog', function(req, res) {
-			let email = req.body.email
-			let firstname = req.body.firstname
-			let lastname = req.body.lastname
-			let age = req.body.age
-			let dogowner = req.body.dogowner
-			let streetAddress =req.body.streetAddress
-			let homeNumber = req.body.homeNumber
-			let city = req.body.city
-			let postCode = req.body.postCode
-			let phoneNumber = req.body.phoneNumber
 			let dogname = req.body.dogname
 			let aboutDog = req.body.about
 			let smallDog = req.body.smallDogButton
 			let mediumDog = req.body.mediumDogButton
 			let largeDog = req.body.largeDogButton
-			req.session.dogname = dogname
-			req.session.aboutDog = aboutDog
-			req.session.smallDog = smallDog
-			req.session.mediumDog = mediumDog
-			req.session.largeDog = largeDog
-			res.render("signupCheck", {email: req.session.email, firstname: req.session.firstname,
-			 			lastname: req.session.lastname, age: req.session.age, dogowner: req.session.dogowner,
-			  			streetAddress: req.session.streetAddress, homeNumber: req.session.homeNumber,
-			  			city: req.session.city, postCode: req.session.postCode, phoneNumber: req.session.phoneNumber,
-			  			dogname: req.session.dogname, aboutDog: req.session.aboutDog, smallDog: req.session.smallDog, 
-			  			mediumDog: req.session.mediumDog, largeDog: req.session.largeDog})
+			// let pictureDog = // let picture = Store picture somewhere
 		})
-
-		app.post('/signupCheck', function (req, res) {
-			let email = req.body.email
-			let firstname = req.body.firstname
-			let lastname = req.body.lastname
-			let age = req.body.age
-			let dogowner = req.body.dogowner
-			let streetAddress =req.body.streetAddress
-			let homeNumber = req.body.homeNumber
-			let city = req.body.city
-			let postCode = req.body.postCode
-			let phoneNumber = req.body.phoneNumber
-			let username = req.body.username
+		app.post('/signupFormDoglover', function(req, res) {
 			let aboutMe = req.body.about
 			let smallDog = req.body.smallDogButton
 			let mediumDog = req.body.mediumDogButton
 			let largeDog = req.body.largeDogButton
-			// let picture = Store picture somewhere
-
+			// let profilePicture = // let picture = Store picture somewhere
 		})
 	}
 
