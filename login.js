430bf45186f46012992201e58a8f15362cd10afc
Ricky Waldron <ricky_waldron@hotmail.com>
2017-12-21 11:36:19 +0100

    workingPicturesLoginMatch

--- login.js
+++ login.js
@@ -20,7 +20,7 @@ module.exports = (app, client) => {
             	if (error) throw error
             	if (result.rows.length == 1){
             		req.session.email = email
-            		res.redirect('match')
+            		res.redirect('profile')
             	}
             	else {
             		res.send("No user exists with this username")
