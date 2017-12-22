46bd58f6155e60960005ebdb30fe814dbaa85bce
Ricky Waldron <ricky_waldron@hotmail.com>
2017-12-22 16:08:07 +0100

    lastAdds

--- matching.js
+++ matching.js
@@ -22,17 +22,25 @@ module.exports = (app, client) => {
 	app.get("/matchPersons", (req, res) => {
 		if(req.session.email){
 		let email = req.session.email
-		client.query(`SELECT size, id FROM users WHERE email='${email}'`, (error, result) => {
+		client.query(`SELECT size, id, dogowner FROM users WHERE email='${email}'`, (error, result) => {
 			if (error) throw error
-				userId = result.rows[0].id
+				dogOwner = result.rows[0].dogowner
 				preferredSize = result.rows[0].size
-			client.query(`SELECT * FROM users WHERE size='${preferredSize}' AND id !='${userId}'`, (error, result) => {
-				let allMatches = []
-				console.log(result.rows)
-				allMatches.push(result)
-				res.render("matchPersons", {email: req.session.email, allMatches: allMatches})
-				})
-			})
+				userId = result.rows[0].id
+				if (dogOwner == true) {
+					req.session.dogowner = dogOwner
+					client.query(`SELECT * FROM users WHERE size='${preferredSize}' AND id !='${userId}'`, (error, result) => {
+					let allMatches = []
+					console.log(result.rows)
+					allMatches.push(result)
+					res.render("matchPersons", {email: req.session.email, allMatches: allMatches, dogOwner: req.session.dogowner})
+					})
+				}
+				else if(dogOwner == false) {
+						req.session.dogowner = dogOwner
+						res.render("matchPersons", {email: req.session.email, dogOwner: req.session.dogowner})
+					}
+			})		
 		}
 	else {
 			res.redirect("/")
